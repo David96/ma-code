8cc5741ea94f9cb16b0835562df45ffcf703162b
David96 <david@hameipe.de>
2021-06-25 13:18:24 +0200

    Add sensitivity plot & fix some plotting bugs
    plot_trace_back didn't add the shift and therefore plotted for the wrong
    frequency…

--- fixed_disks/plot_stuff.jl
+++ fixed_disks/plot_stuff.jl
@@ -2,6 +2,7 @@ include("FileUtils.jl")
 include("BoostFactorOptimizer.jl")
 
 using BoostFractor
+using Glob
 using PyPlot
 
 #freq = ARGS[1]
@@ -10,7 +11,7 @@ using PyPlot
 
 # %%
 
-freq = "2.2e10"
+freq = "1.5e10"
 shift = "5.0e7"
 version = "v1"
 
@@ -28,7 +29,8 @@ freq_center = parse(Float64, freq)
 freq_range = (freq_center - 0.5e9):0.004e9:(freq_center + 0.5e9)
 
 eps = vcat(1e20, reduce(vcat, [1, epsilon] for i in 1:n_disk), 1)
-init_spacings = read_optim_spacing_from_file("results/optim_$(freq)_2021-04-26.txt")
+#init_spacings = read_optim_spacing_from_file("results/optim_$(freq)_2021-04-26.txt")
+init_spacings = read_optim_spacing_from_file("results/optim_$(freq)_v1.txt")
 
 distances = distances_from_spacing(init_spacings)
 
@@ -38,15 +40,19 @@ optim_params = init_optimizer(n_disk, epsilon, 0.15, 1, 0, freq_center, 50e6, fr
 # %%
 shift_text(shift) = shift < 0 ? "$(shift)" : "+$(shift)"
 
-function plot_boostfactors()
+function plot_boostfactors(;shifted=true)
     eout_0 = calc_eout(optim_params, zeros(n_disk))[1, 1, :]
+    xlabel("Frequency [GHz]")
+    ylabel("Boostfactor")
     plot(freq_range .* 1e-9, abs2.(eout_0))
 
-    for i in 1:n_disk
-        optim_spacings = read_optim_spacing_from_file(
-                          "results/optim_$freq$(shift_text(shift))_f$(i)_$version.txt")
-        plot(freq_range .* 1e-9,
-             abs2.(calc_eout(optim_params, optim_spacings, fixed_disk = i)[1, 1, :]))
+    if shifted
+        for i in 1:n_disk
+            optim_spacings = read_optim_spacing_from_file(
+                              "results/optim_$freq$(shift_text(shift))_f$(i)_$version.txt")
+            plot(freq_range .* 1e-9,
+                 abs2.(calc_eout(optim_params, optim_spacings, fixed_disk = i)[1, 1, :]))
+        end
     end
 end
 
@@ -57,6 +63,7 @@ function plot_boostfactor(shift, fixed_disk, ax)
     else
         p.freq_range = (freq_center - 100e6):0.004e9:(freq_center + shift + 100e6)
     end
+    println("Calculating eout_0")
     eout_0 = calc_eout(p, zeros(n_disk))[1, 1, :]
     ax.plot(p.freq_range .* 1e-9, abs2.(eout_0))
 
@@ -70,6 +77,7 @@ function plot_boostfactor(shift, fixed_disk, ax)
         ax.plot(p.freq_range .* 1e-9, abs2.(calc_eout(p, zeros(n_disk))[1, 1, :]))
         update_distances(p, distances, update_itp=false)
     end
+    println("Calculating shifted eout")
     ax.plot(p.freq_range .* 1e-9,
          abs2.(calc_eout(p, optim_spacings, fixed_disk = fixed_disk)[1, 1, :]))
     ax.legend(["\$f_0\$", "Free", "Fixed disk $fixed_disk"])
@@ -136,17 +144,13 @@ function plot_bf_quality(fixed_disks, shift_range; area=true)
                 s = shift_text(shift)
                 optim_spacings = read_optim_spacing_from_file(
                                     "results/optim_$freq$(s)_f$(fixed_disk)_$version.txt")
-                try
-                    cost = calc_real_bf_cost(optim_params, optim_spacings, fixed_disk = fixed_disk,
-                                             area=area)
-                    push!(qualities[i], cost / cost_0)
-                catch e
-                    println("Shift $s, fd $fixed_disk failed: ", e)
-                end
+                cost = calc_real_bf_cost(optim_params, optim_spacings, fixed_disk = fixed_disk,
+                                         area=area)
+                push!(qualities[i], cost / cost_0)
             end
         end
     end
-    figure().set_size_inches(10, 8)
+    figure().set_size_inches(15, 12)
     ax = subplot(1, 1, 1)
     #println(qualities)
     for quality in qualities
@@ -160,24 +164,61 @@ function plot_bf_quality(fixed_disks, shift_range; area=true)
     #grid(xdata=shift_range .* 1e-6, ydata=0.65:0.05:1.15)
     ax.grid(which="minor", alpha=0.2)
     ax.grid(which="major", alpha=0.5)
-    ax.set_ylim(0.65, 1.05)
+    ax.set_ylim(0.55, 1.10)
 end
 
 function plot_trace_back(fixed_disk, freq, shift)
-    optim_spacings = read_optim_spacing_from_file("results/optim_$(freq)$(shift_text(shift))_f$(fixed_disk)_$version.txt")
+    if fixed_disk == 0 && shift == 0
+        optim_spacings = read_optim_spacing_from_file("results/optim_$(freq)_$version.txt")
+    else
+        optim_spacings = read_optim_spacing_from_file("results/optim_$(freq)$(shift_text(shift))_f$(fixed_disk)_$version.txt")
+    end
     if fixed_disk != 0
         optim_spacings = spacings_with_fd(optim_spacings, fixed_disk)
     end
     bdry = copy_setup_boundaries(optim_params.sbdry_init, optim_params.coords)
-    bdry.distance[2:2:end-2] .+= optim_spacings
+    if fixed_disk == 0 && shift == 0
+        bdry.distance[2:2:end-2] = optim_spacings
+    else
+        bdry.distance[2:2:end-2] .+= optim_spacings
+    end
     p = deepcopy(optim_params)
-    p.freq_range = freq
+    p.freq_range = freq + shift
     # set fixed_disk to 0 here as we already inserted the missing distance
     refl = calc_eout(p, optim_spacings, fixed_disk=0, reflect=true)[1][1]
     full_fields = BoostFractor.transformer_trace_back(refl, p.m_reflect, bdry, p.coords, p.modes,
-                                         prop=propagator1D, f=freq)
+                                         prop=propagator1D, f=freq + shift)
     figure().set_size_inches(15, 12)
-    plot_1d_field_pattern(-autorotate(full_fields[:,:,1]), bdry, freq)
+    plot_1d_field_pattern(-autorotate(full_fields[:,:,1]), bdry, freq + shift)
+end
+
+function plot_sensitivity(;area=false)
+    freqs = Dict{Float64, Float64}()
+    for f in glob([r"optim_[^+-]+_v1.txt"], "results")
+        freq_match = match(r"results\/optim_([^+-]+)_v1.txt", f)
+        freq = parse(Float64, freq_match.captures[1])
+        spacings = read_optim_spacing_from_file(f)
+        f_range = (freq - 0.5e9):0.004e9:(freq + 0.5e9)
+        distances = distances_from_spacing(spacings)
+        p = init_optimizer(n_disk, epsilon, 0.15, 1, 0, freq, 50e6, f_range,
+                                      distances, eps)
+        cost_0 = calc_real_bf_cost(p, zeros(n_disk), fixed_disk=0, area=area)
+        freqs[freq] = cost_0
+    end
+    f = Array{Float64}(undef, 0)
+    b = Array{Float64}(undef, 0)
+    for key in sort!(collect(keys(freqs)))
+        append!(f, key)
+        append!(b, freqs[key])
+    end
+    bfs_norm = b ./ minimum(b)
+    plot(f, bfs_norm)
+    println(f)
+    xticks(1e10:1e9:3e10)
+    grid()
+    #xticks(1e10:0.5e9:3e10, minor=true)
+    #grid(which="minor", alpha=0.2)
+    #grid(which="major", alpha=0.5)
 end
 
 """
@@ -223,7 +264,7 @@ end
 """
     Plot the fields inside one region for a 1D calculation
 """
-function plot_region_1d(R, L, z0, z1, k; bgcolor=nothing, extraspace=false,fill=false, Ea=0,maxE=10)    
+function plot_region_1d(R, L, z0, z1, k; bgcolor=nothing, extraspace=false,fill=false, Ea=0,maxE=10)
     # Construct the relative coordinate system for that region
     z1 += 1e-9
     maximum = (z1+(extraspace ? 10e-3 : 0))
