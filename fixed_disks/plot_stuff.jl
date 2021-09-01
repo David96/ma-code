1548ca2406046f84f1529643461666b5774db2bc
David96 <david@hameipe.de>
2021-09-01 10:07:25 +0200

    [fixed_disks] cosmetic change of sensitivity plot

--- fixed_disks/plot_stuff.jl
+++ fixed_disks/plot_stuff.jl
@@ -260,7 +260,7 @@ function plot_sensitivity(;area=false)
     if area
         ax.set_ylabel("Boostfactor magnitude")
     else
-        ax.set_ylabel("-C")
+        ax.set_ylabel("- C")
     end
     ax.set_xticks((f[1] / 1e9):2.0:(f[end] / 1e9))
     ax.set_xticks((f[1] / 1e9):0.5:(f[end] / 1e9), minor=true)
